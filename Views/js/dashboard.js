163ff5108c0bcef655a95c9ecd19a044c130185f
Purushotam Adhikari <purugcit79@gmail.com>
2023-06-07 06:15:51 +0000

    update admin info

--- Views/js/dashboard.js
+++ Views/js/dashboard.js
@@ -30,29 +30,3 @@ fetch('https://cordychain.onrender.com/api/v1/users').then((jsonData)=>{
 
 })
 
-// import {showAlert} from './alert.js'
-// //logging out
-// const logout = async () => {
-//     try{
-//         const res = await axios({
-//             method: "POST",
-//             url: "https://cordychain.onrender.com/api/v1/users/logout",
-
-//         })
-//         if (res.data.status === 'success') {
-//             location.reload(true)
-//         }
-//     }catch(err){
-//         showAlert('error', 'Error logging out! try again.')
-//     }
-// }
-// var obj
-// if (document.cookie) {
-//     obj = JSON.parse(document.cookie.substring(6))
-// }else {
-//     obj = JSON.parse('{}')
-// }
-
-// var doc = document.querySelector('#logout')
-
-//   doc.addEventListener('click', (e) => logout())
